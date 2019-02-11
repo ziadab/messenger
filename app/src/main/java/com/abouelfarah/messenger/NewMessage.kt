050c494dd4c16766903e40baa9943b7b43fab573
Ziad Abouelfarah <ziadabouelfarah2@gmail.com>
2019-02-11 15:45:50 +0000

    Solving the problem of RecyclerView

--- app/src/main/java/com/abouelfarah/messenger/NewMessage.kt
+++ app/src/main/java/com/abouelfarah/messenger/NewMessage.kt
@@ -2,6 +2,10 @@ package com.abouelfarah.messenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessage : AppCompatActivity() {
 
@@ -10,5 +14,25 @@ class NewMessage : AppCompatActivity() {
         setContentView(R.layout.activity_new_message)
 
         supportActionBar?.title = "Select User"
+
+        val adapter = GroupAdapter<ViewHolder>()
+
+        adapter.add(UserItem())
+
+        recyclerViewer.adapter = adapter
+
+
     }
+}
+
+
+class UserItem(): Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        // xi le"ba
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_new_msg_row
+    }
+
 }
